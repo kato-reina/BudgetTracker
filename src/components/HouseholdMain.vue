456d2f4147ccaa4046ef8b1ea06f8ad1e48f91e5
kato-reina <kato.reina.n8q@jp.nssol.nipponsteel.com>
2024-12-18 11:19:25 +0900

    レビュー内容修正　配置以外

--- src/components/HouseholdMain.vue
+++ src/components/HouseholdMain.vue
@@ -6,34 +6,23 @@
   const formMessage = 'フォームからメッセージ送信';
   const tableMessage = '一覧を表示します';
   const pageSelect = ref(true);
-
-  const SwitchToForm = () => {
-    pageSelect.value = true;
-  };
-
-  const switchToTable = () => {
-    pageSelect.value = false;
-  };
+  // 今回はtrueでForm表示
+  const setPageSelect = (flag: boolean) => (pageSelect.value = flag);
+  // for css
+  const commonButtonStyle =
+    'text-white font-bold py-2 px-4 border-b-4 active:border-none active:translate-y-1 rounded h-12';
+  const fromButtonStyle = `bg-blue-500 hover:bg-blue-400 active:bg-blue-600 border-blue-700 hover:border-blue-500 ${commonButtonStyle}`;
+  const tableButtonStyle = `bg-green-500 hover:bg-green-400 active:bg-green-600 border-green-700 hover:border-green-500 ${commonButtonStyle}`;
 </script>
 
 <template>
-  <div class="w-[800px] h-[600px]">
+  <div class="w-[1000px] h-[600px]">
     <div class="flex">
       <div class="m-4">
-        <button
-          class="bg-blue-500 hover:bg-blue-400 active:bg-blue-600 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 active:border-none active:translate-y-1 rounded h-12"
-          @click="SwitchToForm"
-        >
-          Formを表示
-        </button>
+        <button :class="fromButtonStyle" @click="setPageSelect(true)">Formを表示</button>
       </div>
       <div class="m-4">
-        <button
-          class="bg-green-500 hover:bg-green-400 active:bg-green-600 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 active:border-none active:translate-y-1 rounded h-12"
-          @click="switchToTable"
-        >
-          Tableを表示
-        </button>
+        <button :class="tableButtonStyle" @click="setPageSelect(false)">Tableを表示</button>
       </div>
     </div>
     <div class="m-4">
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -19,12 +19,26 @@
     row10: number;
   };
 
-  const tableData = ref<TableRow[]>([
-    { key: 1, row1: 10, row2: 20, row3: 30, row4: 40, row5: 50, row6: 60, row7: 70, row8: 80, row9: 90, row10: 100 },
-    { key: 2, row1: 15, row2: 25, row3: 35, row4: 45, row5: 55, row6: 65, row7: 75, row8: 85, row9: 95, row10: 105 },
-    { key: 3, row1: 20, row2: 30, row3: 40, row4: 50, row5: 60, row6: 70, row7: 80, row8: 90, row9: 100, row10: 110 },
-    { key: 4, row1: 25, row2: 35, row3: 45, row4: 55, row5: 65, row6: 75, row7: 85, row8: 95, row9: 105, row10: 115 },
-  ]);
+  const rows = 4;
+  const tableData = ref<TableRow[]>(
+    Array.from({ length: rows }).map((_, rowIndex) => {
+      const baseValue = rowIndex + 1;
+      return {
+        key: baseValue,
+        row1: baseValue,
+        row2: baseValue + rows,
+        row3: baseValue + 2 * rows,
+        row4: baseValue + 3 * rows,
+        row5: baseValue + 4 * rows,
+        row6: baseValue + 5 * rows,
+        row7: baseValue + 6 * rows,
+        row8: baseValue + 7 * rows,
+        row9: baseValue + 8 * rows,
+        row10: baseValue + 9 * rows,
+      };
+    })
+  );
+  const filteredProps = (row: TableRow) => Object.keys(row).filter((prop) => prop !== 'key');
 </script>
 
 <template>
@@ -32,28 +46,14 @@
     <span class="text-gray-700 text-lg font-semibold mb-4 block">{{ props.tableMessage }}</span>
     <table class="min-w-full bg-white">
       <tr>
-        <th class="py-2 px-4 bg-gray-200 text-gray-600 font-bold uppercase text-sm">row1</th>
-        <th class="py-2 px-4 bg-gray-200 text-gray-600 font-bold uppercase text-sm">row2</th>
-        <th class="py-2 px-4 bg-gray-200 text-gray-600 font-bold uppercase text-sm">row3</th>
-        <th class="py-2 px-4 bg-gray-200 text-gray-600 font-bold uppercase text-sm">row4</th>
-        <th class="py-2 px-4 bg-gray-200 text-gray-600 font-bold uppercase text-sm">row5</th>
-        <th class="py-2 px-4 bg-gray-200 text-gray-600 font-bold uppercase text-sm">row6</th>
-        <th class="py-2 px-4 bg-gray-200 text-gray-600 font-bold uppercase text-sm">row7</th>
-        <th class="py-2 px-4 bg-gray-200 text-gray-600 font-bold uppercase text-sm">row8</th>
-        <th class="py-2 px-4 bg-gray-200 text-gray-600 font-bold uppercase text-sm">row9</th>
-        <th class="py-2 px-4 bg-gray-200 text-gray-600 font-bold uppercase text-sm">row10</th>
+        <th v-for="n in 10" :key="n" class="py-2 px-4 bg-gray-200 text-gray-600 font-bold uppercase text-sm">
+          {{ `カテゴリ${n}` }}
+        </th>
       </tr>
       <tr v-for="row in tableData" :key="row.key" class="hover:bg-gray-100">
-        <td class="py-2 px-4 border-b border-gray-200">{{ row.row1 }}</td>
-        <td class="py-2 px-4 border-b border-gray-200">{{ row.row2 }}</td>
-        <td class="py-2 px-4 border-b border-gray-200">{{ row.row3 }}</td>
-        <td class="py-2 px-4 border-b border-gray-200">{{ row.row4 }}</td>
-        <td class="py-2 px-4 border-b border-gray-200">{{ row.row5 }}</td>
-        <td class="py-2 px-4 border-b border-gray-200">{{ row.row6 }}</td>
-        <td class="py-2 px-4 border-b border-gray-200">{{ row.row7 }}</td>
-        <td class="py-2 px-4 border-b border-gray-200">{{ row.row8 }}</td>
-        <td class="py-2 px-4 border-b border-gray-200">{{ row.row9 }}</td>
-        <td class="py-2 px-4 border-b border-gray-200">{{ row.row10 }}</td>
+        <td v-for="prop in filteredProps(row)" :key="prop" class="py-2 px-4 border-b border-gray-200">
+          {{ row[prop as keyof TableRow] }}
+        </td>
       </tr>
     </table>
   </div>
